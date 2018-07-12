38efd471061bf2b2c4169ef72d18e1a15a45975f
cvargasp <cvargasp91@gmail.com>
2018-07-12 15:13:56 -0400

    Fix errors

--- resources/views/detalle_promociones.blade.php
+++ resources/views/detalle_promociones.blade.php
@@ -22,13 +22,22 @@ $promotion = DB::table('promotions')->where('id', $promoId)->first();
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-5 col-md-5 effect-julia">
-				<img src="{{asset('images/'.$promotion->filename)}}">
+				<img src="{{asset('images/'.$promotion->filename)}}" width="100%">
 			</div>
 			<div class="col-lg-7 col-md-7 text-left promo-detail">
 				<h1> {{ $promotion->name }} </h1>
 				<h2> Oferta: ${{ $promotion->promotion_price }} </h2>
-				<h2> Normal: ${{ $promotion->normal_price }} </h2>	
-				<p> {{ $promotion->description }} </p>
+				<h2> Normal: ${{ $promotion->normal_price }} </h2>
+				<p class="promo-detail-include">Incluye:</p>
+				@php
+				echo "<ul class=\"promo-detail-list\">";
+				$incluidos = explode(".", $promotion->description);
+				for($i = 0; $i < count($incluidos)-1;$i++)
+				{
+					echo "<li type=\"disc\">{$incluidos[$i]}</li>";
+				}
+				echo "</ul>";
+				@endphp
 				<!-- {!!html_entity_decode($promotion->description)!!} -->
 			</div>
 		</div>
--- resources/views/preguntas_frecuentes.blade.php
+++ resources/views/preguntas_frecuentes.blade.php
@@ -7,68 +7,68 @@
   			<h1>Preguntas Frecuentes</h1>
 		  	<p>Las preguntas más frecuentes</p>
 		  	<div id="accordion">
-		    	<div class="card">
-		      		<div class="card-header">
+		    	<div class="card text-left">
+		      		<div class="card-header faq-question">
 		      			<i class="fas fa-question-circle icon-contact"></i>
 		        		<a class="card-link" data-toggle="collapse" href="#collapseOne">
 		          			¿Que son los implantes mamarios?
 		        		</a>
 		      		</div>
 		      		<div id="collapseOne" class="collapse" data-parent="#accordion">
-		        		<div class="card-body">
+		        		<div class="card-body faq-answer">
 		          			Son cápsulas blandas de silicona rellenas con gel de silicona o solución salina.
 							Estos implantes se usan para agrandar o modificar la forma de los senos.
 		       			 </div>
 		    		</div>
 		    	</div>
-		    	<div class="card">
-		      		<div class="card-header">
-		      			<i class="fas fa-question-circle icon-contact"></i>
+		    	<div class="card text-left">
+		      		<div class="card-header faq-question">
+		      			<i class="fas fa-question-circle icon-contact faq-question"></i>
 		        		<a class="collapsed card-link" data-toggle="collapse" href="#collapseTwo">
 		        			 ¿Qué consecuencias negativas puede tener el tabaco en una cirugía plástica?
 		     	 		</a>
 		      		</div>
 		      		<div id="collapseTwo" class="collapse" data-parent="#accordion">
-		        		<div class="card-body">
+		        		<div class="card-body faq-answer">
 		          			Fumar estrecha los vasos sanguíneos disminuyendo el suministro sanguíneo a las células, lo que hará que el cuerpo demore más en cicatrizar y que aumente la posibilidad de desarrollar infecciones, necrosis cutánea u otras complicaciones.
 		        		</div>
 		      		</div>
 		    	</div>
-		    	<div class="card">
-		      		<div class="card-header">
-		      			<i class="fas fa-question-circle icon-contact"></i>
+		    	<div class="card text-left">
+		      		<div class="card-header faq-question">
+		      			<i class="fas fa-question-circle icon-contact faq-question"></i>
 		        		<a class="collapsed card-link" data-toggle="collapse" href="#collapseThree">
 		          			¿Es necesario hacerse análisis previos a una cirugía?
 		        		</a>
 		      		</div>
 		      		<div id="collapseThree" class="collapse" data-parent="#accordion">
-		        		<div class="card-body">
+		        		<div class="card-body faq-answer">
 		          			Sí, es necesario hacerse exámenes antes de cualquier procedimiento. El médico le indicará durante la consulta que análisis requiere.
 			       		</div>
 		      		</div>
 		    	</div>
-		    	<div class="card">
-		      		<div class="card-header">
-		      			<i class="fas fa-question-circle icon-contact"></i>
+		    	<div class="card text-left">
+		      		<div class="card-header faq-question">
+		      			<i class="fas fa-question-circle icon-contact faq-question"></i>
 		        		<a class="collapsed card-link" data-toggle="collapse" href="#collapseFour">
 		          			¿Es doloroso someterse a una cirugía plástica?
 		        		</a>
 		      		</div>
 		      		<div id="collapseFour" class="collapse" data-parent="#accordion">
-		        		<div class="card-body">
+		        		<div class="card-body faq-answer">
 							En términos generales cualquier procedimiento de cirugía plástica es muy bien tolerado por los pacientes y el dolor cede con la administración de analgésicos de uso habitual.
 			       		</div>
 		      		</div>
 		    	</div>
-		    	<div class="card">
-		      		<div class="card-header">
-		      			<i class="fas fa-question-circle icon-contact"></i>
+		    	<div class="card text-left">
+		      		<div class="card-header faq-question">
+		      			<i class="fas fa-question-circle icon-contact faq-question"></i>
 		        		<a class="collapsed card-link" data-toggle="collapse" href="#collapseFive">
 		          			¿Es seguro realizarse procedimientos de cirugía plástica?
 		        		</a>
 		      		</div>
 		      		<div id="collapseFive" class="collapse" data-parent="#accordion">
-		        		<div class="card-body">
+		        		<div class="card-body faq-answer">
 							Actualmente es muy seguro someterse a procedimientos de cirugía plástica; siempre y cuando sea realizado por un cirujano calificado y en instalaciones hospitalarias que cuenten con toda la infraestructura para poder hacer frente a cualquier incidente durante y después de alguna cirugía, esto es, quirófanos equipados con la más alta tecnología y poder atender alguna urgencia durante ella, terapia intensiva, laboratorios de análisis clínicos, entre otros. En pocas palabras una clínica donde se pueda hacer frente a cualquier eventualidad o complicación médica. 
 			       		</div>
 		      		</div>
