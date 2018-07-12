92a2295fecd4872665979da11cb5e9c2e7ce900e
cvargasp <cvargasp91@gmail.com>
2018-07-12 14:46:24 -0400

    Revert "Correcciones tamaño/formato"
    This reverts commit b3cdc5faa883590a5728914778b71540b54af62b.

--- resources/views/detalle_promociones.blade.php
+++ resources/views/detalle_promociones.blade.php
@@ -22,22 +22,13 @@ $promotion = DB::table('promotions')->where('id', $promoId)->first();
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-5 col-md-5 effect-julia">
-				<img src="{{asset('images/'.$promotion->filename)}}" width="100%">
+				<img src="{{asset('images/'.$promotion->filename)}}">
 			</div>
 			<div class="col-lg-7 col-md-7 text-left promo-detail">
 				<h1> {{ $promotion->name }} </h1>
 				<h2> Oferta: ${{ $promotion->promotion_price }} </h2>
-				<h2> Normal: ${{ $promotion->normal_price }} </h2>
-				<p class="promo-detail-include">Incluye:</p>
-				@php
-				echo "<ul class=\"promo-detail-list\">";
-				$incluidos = explode(".", $promotion->description);
-				for($i = 0; $i < count($incluidos)-1;$i++)
-				{
-					echo "<li type=\"disc\">{$incluidos[$i]}</li>";
-				}
-				echo "</ul>";
-				@endphp
+				<h2> Normal: ${{ $promotion->normal_price }} </h2>	
+				<p> {{ $promotion->description }} </p>
 				<!-- {!!html_entity_decode($promotion->description)!!} -->
 			</div>
 		</div>
